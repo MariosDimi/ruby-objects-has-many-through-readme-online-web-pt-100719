21cd49f7d70e0157bb4114e4084d3d887b1f9054
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 03:39:41 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,5 +17,20 @@ class Customer
     Meal.all.select do |meal|
       meal.customer == self
     end
-  
+    
+  def waiters
+    meals.map do |meal|
+      meal.waiter 
+    end
 end
+
+alex = Customer.new("Alex", 30)
+rahcel = Customer.new("Rachel", 27)
+dan = Waiter.new("Dan", 3)
+
+rachel.new_meal(dan, 50, 10)
+alex.new_meal(dan, 30, 5)
+
+rachel.meals
+rachel.meals.length 
+Meal.all.length
